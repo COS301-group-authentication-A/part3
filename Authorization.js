5f00cf24fcfb18ec619f00f27797b60c76833b1a
Shaun-Cartoo <u11072157@tuks.co.za>
2015-03-19 23:32:35 +0200

    added getAuthorization implementation; take note of the comments i added on servicePoints and my function.

--- Authorization.js
+++ Authorization.js
@@ -1,11 +1,12 @@
 var servicePointsSize=3;
 var servicePoints;
-servicePoints.service[0] = "addAuthorizationRestriction";
+servicePoints.service[0] = "addAuthorizationRestriction";//aren't theses supposed to be the serrvices we restrict? i.e post,comment,upload file,etc. [Shaun]
 servicePoints.service[1] = "removeAuthorizationRestriction";
 servicePoints.service[2] = "getAuthorizationRestrictions";
 servicePoints.points[0] = 0;
 servicePoints.points[1] = 0;
 servicePoints.points[2] = 0;
+
 function updateAuthorisationRestriction(username,restrictionName,level)
 {
     if(!isAuthorized(username,"updateAuthorisationRestriction"))
@@ -18,9 +19,23 @@ function updateAuthorisationRestriction(username,restrictionName,level)
     }
     return false;
 }
+
 function addAuthorisationRestriction(username,restrictionName,level)
 {
     if(!isAuthorized(username,"addAuthorisationRestriction"))
         throw "NotAuthorizedExeption";
     return updateAuthorisationRestriction(username,restrictionName,level);
+}
+
+function getAuthorizationRestriction(restriction)//my function is supposed to return the restriction that can be updated. i.e if user is restricted to post, then i return this authority (postRestriction)so it can be updated. [Shaun]
+{
+    var currRestriction;
+
+    for(var i= 0;i<servicePointsSize;i++)
+        if (restriction == servicePoints.sevice[i]) {
+            currRestriction = servicePoints.sevice[i];
+            break;
+        }
+
+    return currRestriction;
 }
