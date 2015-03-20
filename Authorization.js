5dd6b87f1c42850d34dda738b9469274211c4d09
Charles Williamson <charles.williamson3@gmail.com>
2015-03-20 18:51:19 +0200

    added the classes to the file will edit my class/function later

--- Authorization.js
+++ Authorization.js
@@ -1,47 +1,20 @@
-var servicePointsSize=3;
-var servicePoints;
-servicePoints.service[0] = "addAuthorizationRestriction";//aren't theses supposed to be the serrvices we restrict? i.e post,comment,upload file,etc. [Shaun]
-servicePoints.service[1] = "removeAuthorizationRestriction";
-servicePoints.service[2] = "getAuthorizationRestrictions";
-servicePoints.points[0] = 0;
-servicePoints.points[1] = 0;
-servicePoints.points[2] = 0;
-
-function updateAuthorisationRestriction(username,restrictionName,level)
+var Authorization=fyunction()
 {
-    if(!isAuthorized(username,"updateAuthorisationRestriction"))
-        throw "NotAuthorizedExeption";
-    for(var i= 0;i<servicePointsSize;i++) {
-        if (restrictionName == servicePoints.sevice[i]) {
-            servicePoints.points[i] = level;
-            return true;
+    varservicePointsSize = 3;
+
+    function updateAuthorisationRestriction(UpdateAuthorizationRestrictionRequest) {
+        var UpdateAuthorizationRestriction;
+        if (!isAuthorized(username, "updateAuthorisationRestriction"))
+            throw "NotAuthorizedExeption";
+        for (var i = 0; i < servicePointsSize; i++) {
+            if (updateAuthorizationRestrictionRequest.restrictionName == servicePoints.sevice[i]) {
+                servicePoints.points[i] = level;
+                UpdateAuthorizationRestriction.isUpdated = true;
+                return UpdateAuthorizationRestriction;
+            }
         }
+
+        UpdateAuthorizationRestriction.isUpdated = false;
+        return UpdateAuthorizationRestriction;
     }
-    return false;
-}
-
-function addAuthorisationRestriction(username,restrictionName,level)
-{
-    if(!isAuthorized(username,"addAuthorisationRestriction"))
-        throw "NotAuthorizedExeption";
-    return updateAuthorisationRestriction(username,restrictionName,level);
-}
-
-function removeAuthorizationRequest(username, restrictionName, level){
-	if(!isAuthorized(username,"removeAuthorisationRestriction"))
-        	throw "NotAuthorizedExeption";
-	return updateAuthorisationRestriction(username,restrictionName,0);
-}
-
-function getAuthorizationRestriction(restriction)//my function is supposed to return the restriction that can be updated. i.e if user is restricted to post, then i return this authority (postRestriction)so it can be updated. [Shaun]
-{
-    var currRestriction;
-
-    for(var i= 0;i<servicePointsSize;i++)
-        if (restriction == servicePoints.sevice[i]) {
-            currRestriction = servicePoints.sevice[i];
-            break;
-        }
-
-    return currRestriction;
 }
