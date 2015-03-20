f05f9218b70eb4c44c631138240edeeafb00f3cf
Charles Williamson <charles.williamson3@gmail.com>
2015-03-20 22:22:38 +0200

    changed too proper classes my function is still incorrect

--- Authorization.js
+++ Authorization.js
@@ -1,22 +1,32 @@
-var Authorization=fyunction()
+var Authorization;
+Authorization = function () {//Authorization class
+
+};
+Authorization.prototype.updateAuthorisationRestriction=function(UpdateAuthorizationRestrictionRequest)//The  updateAuthorisationRestriction function
 {
-    varservicePointsSize = 3;
-
-    function updateAuthorisationRestriction(UpdateAuthorizationRestrictionRequest) {
-        var UpdateAuthorizationRestriction;
-        if (!isAuthorized(username, "updateAuthorisationRestriction"))
-            throw "NotAuthorizedExeption";
-        for (var i = 0; i < servicePointsSize; i++) {
-            if (updateAuthorizationRestrictionRequest.restrictionName == servicePoints.sevice[i]) {
-                servicePoints.points[i] = level;
-                UpdateAuthorizationRestriction.isUpdated = true;
-                return UpdateAuthorizationRestriction;
-            }
+    if (!isAuthorized(updateAuthorisationRestrictionRequest.getuserID(), "updateAuthorisationRestriction"))
+        throw NotAuthorizedExeption;
+    for (var i = 0; i < servicePointsSize; i++) {
+        if (updateAuthorizationRestrictionRequest.get == servicePoints.sevice[i]) {
+            servicePoints.points[i] = level;
+            UpdateAuthorizationRestriction.isUpdated = true;
+            return UpdateAuthorizationRestriction;
         }
-
-        UpdateAuthorizationRestriction.isUpdated = false;
-        return UpdateAuthorizationRestriction;
     }
+
+    UpdateAuthorizationRestriction.isUpdated = false;
+    return UpdateAuthorizationRestriction;
+};
+
+var AuthorizationRestriction=function()//used by everyone
+{
+};
+var ServiceRestriction=function()//used by everyone
+{
+};
+var ServiceIdentifier=function()//used by everyone
+{
+};
 
     function addAuthorizationRestriction(AddAuthorizationRestrictionRequest)
     {
@@ -28,4 +38,3 @@ var Authorization=fyunction()
         addAuthorizationRestriction.isAdded = updateAuthorisationRestriction(userId, "addAuthorisationRestriction").isUpdated;
         return addAuthorizationRestriction;
     }
-}
