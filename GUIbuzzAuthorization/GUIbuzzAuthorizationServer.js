5e37da2c65f23b2acfea4660aefb50651ca15a42
Shaun-Cartoo <u11072157@tuks.co.za>
2015-04-01 19:44:53 +0200

    update on GUI, now the dropdown menu gets populated from the database :)

--- GUIbuzzAuthorization/GUIbuzzAuthorizationServer.js
+++ GUIbuzzAuthorization/GUIbuzzAuthorizationServer.js
@@ -13,65 +13,65 @@ http.createServer(function(request, response)
 
             var  i = 0;
             for (i = 0; i < getAuthorizationResult.length;) {
-                cursorObject = cursorObject + "<li>" + getAuthorizationResult[i].methodName + "</li>";
+                cursorObject = cursorObject + "<option onSelect='selectedRestriction("+getAuthorizationResult[i].methodName+");' value='"+getAuthorizationResult[i].methodName+"'>" + getAuthorizationResult[i].methodName + "</option>";
                 i = i + 1;
             }
-
             console.log(cursorObject);
+
+            response.write("<!DOCTYPE html>"
+            +"<head>"
+            +"<meta charset='utf-8'>"
+            +"<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
+            +"<meta name='viewport' content='width=device-width, initial-scale=1'>"
+            +"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>"
+            +"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css'>"
+            +"<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>"
+            +"<script> function addRestriction(){alert('Restriction added!')};"
+            +"function removeRestriction(){alert('Restriction removed!')};"
+            +"function updateRestriction(){alert('Restriction updated!')};"
+            +"function selectedRestriction(option){"
+            +"var val = option.options[option.selectedIndex].value;"
+            +"document.getElementsByTagId('exampleInputEmail3')[0].setAttribute('placeholder', 'ok');"
+            +"};"
+            +"</script>"
+            +"</head>"
+            +"<body>"
+            +"<div class='container'>"
+            +"<div class='starter-template'>"
+            +"<h1>Buzz System</h1>"
+            +"<h2>buzzAuthorisation</h2>"
+            +"<label for='inputEmail' class='sr-only'>UserId</label><input type='text' id='inputEmail' class='form-control' placeholder='i.e u1234567' required autofocus><br/>"
+            +"<p class='lead'>Select a restriction from the dropdown</p>"
+            +"<div class='dropdown'>"
+                +"<select name='Restrictions' onChange='selectedRestriction(this)' class='btn btn-default dropdown-toggle' type='button' id='menu1' data-toggle='dropdown'>"
+                +"<optgroup label='Restrictions'>"
+                    +""+cursorObject+""
+                +"</optgroup>"
+                +"</select>"
+            +"</div>"
+            +"<div style='margin-top:10.7%;'>"
+            +"<form class='form-inline'>"
+            +"<div class='form-group'>"
+            +"<label class='sr-only' for='exampleInputEmail3'>Ranking</label>"
+            +"<input name='restrictionVal' type='text' class='form-control' id='exampleInputEmail3' placeholder='Ranking'>"
+            +"</div>"
+            +"<button type='submit' class='btn btn-primary' onclick='addRestriction();'>Add Restriction</button>"
+            +"</form>"
+            +"</div>"
+            +"<div style='margin-left:25.5%;margin-top:-3%;'>"
+            +"<button type='button' class='btn btn-success' onclick='removeRestriction();'>Remove Restriction</button>"
+            +"<button type='button' class='btn btn-info' onclick='updateRestriction();'>Update Restriction</button>"
+            +"</div>"
+            +"</div>"
+            +"</div>"
+            +"</body>"
+            +"</html>");
+            response.end();
         }
         else
         {
             console.log("There is an error: "+err);
         }
     });
-
-	response.write("<!DOCTYPE html>"
-				+"<head>"
-				+"<meta charset='utf-8'>"
-				+"<meta http-equiv='X-UA-Compatible' content='IE=edge'>"
-				+"<meta name='viewport' content='width=device-width, initial-scale=1'>"
-				+"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>"
-				+"<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css'>"
-				+"<script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>"
-				+"<script> function addRestriction(){alert('Restriction added!')};"
-                +"function removeRestriction(){alert('Restriction removed!')};"
-                +"function updateRestriction(){alert('Restriction updated!')};"
-                +"</script>"
-                +"</head>"
-					+"<body>"
-						+"<div class='container'>"
-							+"<div class='starter-template'>"
-							+"<h1>Buzz System</h1>"
-							+"<h2>buzzAuthorisation</h2>"
-							+"<label for='inputEmail' class='sr-only'>UserId</label><input type='text' id='inputEmail' class='form-control' placeholder='i.e u1234567' required autofocus><br/>"
-							+"<p class='lead'>Select a restriction from the dropdown</p>"
-								+"<div class='dropdown'>"
-									+"<button class='btn btn-default dropdown-toggle' type='button' id='dropdownMenu1' data-toggle='dropdown' aria-expanded='true'>"
-									+"Dropdown"
-									+"<span class='caret'></span>"
-									+"</button>"
-									+"<ul class='dropdown-menu' role='menu' aria-labelledby='dropdownMenu1'>"
-                                    +""+cursorObject.toString()+""
-									+"</ul>"
-								+"</div>"
-                                +"<p id='para1'>Output :"+cursorObject.toString()+"</p>"
-								+"<div style='margin-top:10.7%;'>"
-									+"<form class='form-inline'>"
-										+"<div class='form-group'>"
-										+"<label class='sr-only' for='exampleInputEmail3'>Ranking</label>"
-										+"<input type='email' class='form-control' id='exampleInputEmail3' placeholder='Ranking'>"
-										+"</div>"
-										+"<button type='submit' class='btn btn-primary' onclick='addRestriction();'>Add Restriction</button>"
-									+"</form>"
-								+"</div>"
-								+"<div style='margin-left:25.5%;margin-top:-3%;'>"
-									+"<button type='button' class='btn btn-success' onclick='removeRestriction();'>Remove Restriction</button>"
-									+"<button type='button' class='btn btn-info' onclick='updateRestriction();'>Update Restriction</button>"
-								+"</div>"
-							+"</div>"
-						+"</div>"
-					+"</body>"
-			+"</html>");
-	response.end();
 }).listen(8080);
 console.log('Server is listening to http://localhost/ on port 8080�');
